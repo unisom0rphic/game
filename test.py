da38b6b20f555342f408e71fed7d876545f69f7e
unisom0rphic <117302827+unisom0rphic@users.noreply.github.com>
2025-04-02 18:55:54 +0500

    feat: added status bar (bug: player can go into it lmao)

--- test.py
+++ test.py
@@ -31,30 +31,19 @@ class Player:
         self.health = 100
         print("PLAYER IS CREATED")
 
+# Functions
 def create_background(background_tile: pygame.Surface) -> pygame.Surface:
     background = pygame.Surface((WIDTH, HEIGHT))
     for x in range(0, WIDTH, TILE_SIZE):
         for y in range(0, HEIGHT, TILE_SIZE):
             background.blit(background_tile, (x,y))
-
     return background
 
 
-# Functions
 def render_tile_plain(screen: pygame.Surface, color: pygame.color.Color, x: int, y: int) -> None:
     new_surface = pygame.Surface((TILE_SIZE,TILE_SIZE))
     new_surface.fill(color)
     screen.blit(new_surface, (x*TILE_SIZE, y*TILE_SIZE))
-
-# TODO: test 
-def check_collision(surf1: pygame.Surface, surf2: pygame.Surface) -> bool:
-    rect1 = surf1.get_rect()
-    rect2 = surf2.get_rect()
-    if (rect1.x > rect2.x) and (rect1.x < rect2.x+rect2.width) or \
-            (rect1.y > rect2.y) and (rect1.y < rect2.y+rect2.height):
-            return True
-    else:
-        return False
 
 
 def add_wall(screen: pygame.Surface, color: pygame.color.Color, x: int, y: int, direction: Direction, length: int) -> None:
@@ -73,6 +62,23 @@ def add_wall(screen: pygame.Surface, color: pygame.color.Color, x: int, y: int, 
                 [render_tile_plain(screen, color, x-i, y) for i in range(length)]
 
 
+def check_collision(surf1: pygame.Surface, surf2: pygame.Surface) -> bool:
+    rect1 = surf1.get_rect()
+    rect2 = surf2.get_rect()
+    if (rect1.x > rect2.x) and (rect1.x < rect2.x+rect2.width) or \
+            (rect1.y > rect2.y) and (rect1.y < rect2.y+rect2.height):
+            return True
+    else:
+        return False
+
+
+def update_statusbar(screen: pygame.Surface, player: Player, font: pygame.font.Font) -> None:
+    statusbar = pygame.Surface((WIDTH, 3*TILE_SIZE))
+    statusbar_text = font.render(f'Health: {player.health}', False, WHITE)
+    statusbar.blit(statusbar_text, (0,0))
+    screen.blit(statusbar, (0, HEIGHT-statusbar.get_height()))
+    
+
 
 
 if __name__ == "__main__":
@@ -86,6 +92,7 @@ if __name__ == "__main__":
     
     # Sprites
     FLOOR_IMG = pygame.image.load("sprites/Floor.png").convert_alpha()
+    FONT = pygame.font.Font(None, 30)
      
     wall1 = pygame.rect.Rect(0, 0, TILE_SIZE, TILE_SIZE)
     wall2 = pygame.rect.Rect(0, 0, TILE_SIZE, TILE_SIZE)
@@ -117,10 +124,13 @@ if __name__ == "__main__":
                 elif event.key == pygame.K_DOWN:
                     if (player.pos_y+1 < HEIGHT//TILE_SIZE):
                         player.pos_y += 1
+                elif event.key == pygame.K_d:
+                    if player.health > 5: player.health -= 5
 
 
             render_tile_plain(screen, player.player_color, player.pos_x, player.pos_y)
             add_wall(screen, GRAY, x=7, y=7, direction=Direction.NORTH, length=5)
+            update_statusbar(screen, player, FONT)
 
             pygame.display.update()
             clock.tick(FPS)
